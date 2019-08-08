e497b667ad639a0f3b3a873e7784fd018ae5e564
Laura Colten <lauracolten5@gmail.com>
2019-08-08 15:18:12 -0500

    added seed data

---
+++ Chatter/Models/SeedData.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatter.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ChatterContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ChatterContext>>()))
+            {
+                if (context.Blog.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Blog.AddRange(
+                    new Blog
+                    {
+                        Title = "First Test Blog",
+                        Content = "This is my first test blog from my seed data.",
+                        //Board = new Board(),
+                        
+
+
+                    },
+
+                    new Blog
+                    {
+                        Title = "Second Test Blog",
+                        Content = "This is my second test blog from my seed data.",
+                        //Board = new Board(),
+                      
+
+                    }
+                ); ;
+                context.SaveChanges();
+            }
+        }
+    }
+}
+          
--- Chatter/Program.cs
+++ Chatter/Program.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Chatter.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Chatter;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chatter
 {
@@ -14,11 +18,33 @@ namespace Chatter
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ChatterContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
+
         }
+
+
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
 }
+
